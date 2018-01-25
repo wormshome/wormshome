6fd9aae609fa78df2b91255cf39ceadbaf992e82
wormshome <llwormsll@naver.com>
2018-01-25 19:45:34 +0900

    123
    123

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    var reset = 0;
     $(".box_menu ul li a, .nav ul li a").click(function(){
         var flag = 0;
         if(($("body").attr("class") == undefined) || ($("body").attr("class") == "") ){
@@ -13,25 +14,25 @@ $( document ).ready(function() {
         var idx = $(this).parent().index();
         $(".box_menu ul li").eq(idx).addClass("on");
         if(bg=="profile"){
-            $("body").addClass("bg01");           
+            $("body").addClass("bg01");
         }else if(bg=="project"){
-            $("body").addClass("bg02");           
+            $("body").addClass("bg02");
         }else if(bg=="skill"){
-            $("body").addClass("bg03");           
+            $("body").addClass("bg03");
         }else if(bg=="contact"){
-            $("body").addClass("bg04");           
+            $("body").addClass("bg04");
         }else if(bg=="Study"){
-            $("body").addClass("bg05");           
+            $("body").addClass("bg05");
         }else if(bg=="none"){
-            $("body").addClass("bg06");           
+            $("body").addClass("bg06");
         }
 
-        $(".box_menu ul li").animate({
+        $(".box_menu ul li").stop().animate({
             top:0,
             left:0
         }, flag, function(){
             if(bg=="profile"){
-                $(".box_profile").addClass("on");            
+                $(".box_profile").addClass("on");
             }else if(bg=="project"){
                 $(".box_project").addClass("on");
             }else if(bg=="skill"){
@@ -43,10 +44,32 @@ $( document ).ready(function() {
             }else if(bg=="none"){
                 $(".box_none").addClass("on");
             }
+            reset = 1;
         });
     });
     $(".logo a").click(function(){
-        $("body").attr("class" , "");
-        $(".container .inner > div").removeClass("on");
+        if(reset == 1){
+            $("body").attr("class" , "");
+            $(".container .inner > div").removeClass("on");
+            $(".box_menu ul li").removeClass("on");
+
+            for(var i = 1; i < 6; i++){
+                $(".box_menu ul li").eq(i).stop().animate({
+                    top:i < 3 ? 0 : 340,
+                    left:(i%3) * 340,
+                },1000);
+            }
+            reset = 0;
+        };               
+    });
+    $(".box_contents li").mouseenter(function(){
+        $(this).children(".sub_menu").stop().animate({
+            top:0,
+        },300);
+    });
+    $(".box_contents li").mouseleave(function(){
+        $(this).children(".sub_menu").stop().animate({
+            top:-100,
+        },500);
     });
 });
